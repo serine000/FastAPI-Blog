637926699b56458d0325b88b0ab254ac2e83def9
serine <serine_md@hotmail.com>
2023-09-14 12:28:50 +0300

    adding login authentication

--- backend/apis/base.py
+++ backend/apis/base.py
@@ -9,3 +9,4 @@ from fastapi import APIRouter
 api_router = APIRouter()
 api_router.include_router(route_user.router, prefix="", tags=["users"])
 api_router.include_router(route_blog.router, prefix="", tags=["blogs"])
+api_router.include_router(route_blog.router, prefix="", tags=["login"])
